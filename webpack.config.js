53e50b32974f103bc5b84dae148b847f5538e05d
noysealizer <hatzlgerhard@gmail.com>
2019-03-20 16:14:26 +0100

    one change in webpack.config and two in packkage.json

--- webpack.config.js
+++ webpack.config.js
@@ -4,31 +4,28 @@ let entry = __dirname + "/app/src/page.js";
 let outputPath = __dirname + "/dist/";
 let devtool = "";
 if (process.env.TESTBUILD) {
-  entry = glob.sync(__dirname + "/app/test/**/*.test.js");
-  outputPath = __dirname + "/test-dist/";
-  devtool = "source-map";
+    entry = glob.sync(__dirname + "/app/test/**/*.test.js");
+    outputPath = __dirname + "/test-dist/";
+    devtool = "source-map";
 }
 
 module.exports = {
-  entry: entry,
-  output: {
-    path: outputPath,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: ["/node_modules/"],
-        use: [
-          {
-            loader: "babel-loader",
-            options: {
-              presets: ["env"],
-            },
-          },
-        ],
-      },
-    ],
-  },
-  devtool: devtool,
+    mode: "development",
+    entry: entry,
+    output: {
+        path: outputPath,
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: ["/node_modules/"],
+            use: [{
+                loader: "babel-loader",
+                options: {
+                    presets: ["env"],
+                },
+            }, ],
+        }, ],
+    },
+    devtool: devtool,
 };
